8636a64e63909da60477523c7c2312d47d2abf16
Ahalya Mandana <31750142+am2512@users.noreply.github.com>
2018-10-20 11:22:35 -0500

    Merging Ben & Ahalya's functions

--- recyclableOrNot.py
+++ recyclableOrNot.py
@@ -1,9 +1,45 @@
 #!/usr/bin/env python
 # this program takes user input from a keyboard (y/n)
+import sys
+import os
+import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
+import copy
+import cv2
+import time 
+from datetime import *
+from PIL import Image
 
+def webcam():
+    cap = cv2.VideoCapture(1)
+
+    #setting image size
+    cap.set(3,1280)
+    cap.set(4,1024)
+
+    ret, img = cap.read()
+    img = cv2.flip(img, 1)
+    if ret:
+    # x1, y1, x2, y2 = 175, 175, 375, 375
+    # img_cropped = img[y1:y2, x1:x2]
+    # cv2.imwrite( "/home/ahalyamandana/Desktop/test.jpg", img_cropped);
+    
+        #TODO fix file path, change to relative path
+        filename = "datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+        path = "/home/ahalyamandana/Desktop/" + filename +".jpg"
+        cv2.imwrite(path, img);
+
+        
+           
+# Following line should appear but is not working with opencv-python package
+    cap.release()
+    cv2.destroyAllWindows
+    return filename
 
 def main():
     lidCondition = "c"
+    string
     while (True):
         #Hold prev condition
         prevLidCondition = lidCondition
@@ -15,7 +51,8 @@ def main():
         if (prevLidCondition != lidCondition and lidCondition == "c"):
 
             #Take image
-            print "Taking image"#Call Ahalya's take image function here
+            webcam()
+            print "Taking image"
             isRecyclable = raw_input("Is this recyclable?\n")
             if (isRecyclable == 'y'):
                 print("This is recyclable\n")
