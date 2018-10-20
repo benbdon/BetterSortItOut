facd7da23a8680d47fc2ffe5913b34ce1a072bc9
Ian Davis <idtx314business@gmail.com>
2018-10-20 11:22:19 -0500

    Added new_log function

--- response_logger.py
+++ response_logger.py
@@ -40,12 +40,18 @@ def log(arg):
     f = open("trash_log.txt","a")
 
     # log time and the string response
-    s = str(datetime.datetime.now()) + " : " + arg + '\n'
+    # s = str(datetime.datetime.now()) + " : " + arg + '\n'
+    s = arg + '\n'
     f.write(s)
 
     f.close()
 
 
+def new_log():
+    f = open("trash_log.txt","w")
+
+    f.write('')
+    f.close()
 
 
 
