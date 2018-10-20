a29026008fd8d2846e8bad0c7107d7d06333442a
Ahalya Mandana <31750142+am2512@users.noreply.github.com>
2018-10-20 10:49:00 -0500

    Script to take a pictre with the webcam

---
+++ webcam.py
@@ -0,0 +1,34 @@
+
+import sys
+import os
+import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
+import copy
+import cv2
+import time 
+from datetime import *
+from PIL import Image
+
+# time.sleep(5)
+
+cap = cv2.VideoCapture(1)
+
+#setting image size
+cap.set(3,1280)
+cap.set(4,1024)
+
+ret, img = cap.read()
+img = cv2.flip(img, 1)
+if ret:
+    # x1, y1, x2, y2 = 175, 175, 375, 375
+    # img_cropped = img[y1:y2, x1:x2]
+    # cv2.imwrite( "/home/ahalyamandana/Desktop/test.jpg", img_cropped);
+    filename1 = "/home/ahalyamandana/Desktop/" + datetime.now().strftime("%Y-%m-%d-%H-%M-%S")+".jpg"
+    cv2.imwrite(filename1, img);
+
+        
+           
+# Following line should appear but is not working with opencv-python package
+cap.release()
+cv2.destroyAllWindows()
